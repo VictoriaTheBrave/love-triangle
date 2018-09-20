aa86aa25abafdbf37d1f6350b85348567304d133
VictoriaTheBrave <v1ktor1a09051994@gmail.com>
2018-09-20 23:16:38 +0300

    love-triangles/feature: implemented function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let first = preferences[i];
+    let second = preferences[first - 1];
+    let third = preferences[second - 1];
+    if (third === i + 1 && i !== first - 1) {
+      delete preferences[i, first - 1, second - 1];
+      count = count + 1;
+    }
+  }
+  return count;
 };
